02d15e5c1b1a8278dc8e1e30dcd8263e88a03f5e
Kirk Wight <kwight@kwight.ca>
2014-03-14 14:34:09 -0700

    Eventbrite: Have Keyring load the Eventbrite service from outside its own extended folder.
    This stops Keyring from just autoloading the Eventbrite service that was originally included. It's a first step towards loading the Eventbrite service from the standalone Keyring plugin, if present.

--- eventbrite-keyring/eventbrite.php
+++ eventbrite-keyring/eventbrite.php
@@ -8,7 +8,7 @@
  */
 
 class Keyring_Service_Eventbrite extends Keyring_Service_OAuth2 {
-  const NAME  = 'eventbrite';
+	const NAME  = 'eventbrite';
 	const LABEL = 'Eventbrite';
 
 	function __construct() {
@@ -52,22 +52,26 @@ class Keyring_Service_Eventbrite extends Keyring_Service_OAuth2 {
 	}
 
 	function keyring_connection_verified( $service, $id, $request_token ) {
-		if ( 'eventbrite' != $service || 'eventbrite' != $request_token->token['for'] )
+		if ( 'eventbrite' != $service || ( isset( $request_token->token['for'] ) && 'eventbrite' != $request_token->token['for'] ) )
 			return;
-
-		update_blog_option( absint( $request_token->token['blog_id'] ), 'eventbrite_token', array( $id ) );
+		if ( is_multisite() )
+			update_blog_option( absint( $request_token->token['blog_id'] ), 'eventbrite_token', array( $id ) );
+		else
+			update_option( 'eventbrite_token', array( $id ) );
 	}
 
 	function keyring_connection_deleted( $service, $request ) {
 		if ( 'eventbrite' != $service )
 			return;
-
-		delete_blog_option( absint( $request['blog'] ), 'eventbrite_token' );
+		if ( is_multisite() )
+			delete_blog_option( absint( $request['blog'] ), 'eventbrite_token' );
+		else
+			delete_option( 'eventbrite_token' );
 	}
 
 	function basic_ui_intro() {
-		echo '<p>' . sprintf( __( "To get started, <a href='https://www.eventbrite.com/api/key'>register an OAuth client on Eventbrite</a>. The most important setting is the <strong>OAuth redirect_uri</strong>, which should be set to <code>%s</code>. You can set the other values to whatever you like.", 'keyring' ), esc_url( Keyring_Util::admin_url( 'eventbrite', array( 'action' => 'verify' ) ) ) ) . '</p>';
-		echo '<p>' . __( "Once you've saved those changes, copy the <strong>APPLICATION KEY</strong> value into the <strong>API Key</strong> field, then click the 'Show' link next to the <strong>OAuth client secret</strong>, copy the value into the <strong>API Secret</strong> field and click save (you don't need an App ID value for Eventbrite).", 'keyring' ) . '</p>';
+		echo '<p>' . sprintf( __( "To get started, <a href='https://www.eventbrite.com/api/key'>register an OAuth client on Eventbrite</a>. The most important setting is the <strong>OAuth redirect_uri</strong>, which should be set to <code>%s</code>. You can set the other values to whatever you like.", 'eventbrite-parent' ), esc_url( Keyring_Util::admin_url( 'eventbrite', array( 'action' => 'verify' ) ) ) ) . '</p>';
+		echo '<p>' . __( "Once you've saved those changes, copy the <strong>APPLICATION KEY</strong> value into the <strong>API Key</strong> field, then click the 'Show' link next to the <strong>OAuth client secret</strong>, copy the value into the <strong>API Secret</strong> field and click save (you don't need an App ID value for Eventbrite).", 'eventbrite-parent' ) . '</p>';
 	}
 
 	function build_token_meta( $token ) {
@@ -80,7 +84,7 @@ class Keyring_Service_Eventbrite extends Keyring_Service_OAuth2 {
 		);
 		$response = $this->request( $this->self_url, array( 'method' => $this->self_method ) );
 		$meta = array();
-		if ( !Keyring_Util::is_error( $response ) ) {
+		if ( ! Keyring_Util::is_error( $response ) ) {
 			if ( isset( $response->user->email ) )
 				$meta['username'] = $response->user->email;
 
@@ -100,7 +104,7 @@ class Keyring_Service_Eventbrite extends Keyring_Service_OAuth2 {
 				$meta['name'] = implode( ' ', $name );
 		}
 
-		return apply_filters( 'keyring_access_token_meta', $meta, 'eventbrite', $token, null, $this );
+		return apply_filters( 'keyring_access_token_meta', $meta, self::NAME, $token, array(), $this );
 	}
 
 	function get_display( Keyring_Access_Token $token ) {
--- eventbrite.php
+++ eventbrite.php
@@ -10,11 +10,33 @@ License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+/**
+ * Load Keyring first.
+ */
 require( 'keyring/keyring.php' );
-require( 'voce-settings-api/voce-settings-api.php' );
-require( 'eventbrite-api/eventbrite-api.php' );
-require( 'eventbrite-settings/eventbrite-settings.php' );
-require( 'eventbrite-widgets/eventbrite-widgets.php' );
-require( 'suggested-pages-setup/suggested-pages-setup.php' );
-require( 'tlc-transients/tlc-transients.php' );
-require( 'php-calendar/calendar.php' );
+
+/**
+ * Load the Eventbrite extended Keyring class.
+ */
+function eventbrite_load_eventbrite_keyring_service() {
+
+	require( 'eventbrite-keyring/eventbrite.php' );
+
+}
+add_action( 'plugins_loaded', 'eventbrite_load_eventbrite_keyring_service' );
+
+/**
+ * Load remaining Eventbrite code after Keyring is initialized.
+ */
+function eventbrite_load_post_keyring() {
+
+	require( 'voce-settings-api/voce-settings-api.php' );
+	require( 'eventbrite-api/eventbrite-api.php' );
+	require( 'eventbrite-settings/eventbrite-settings.php' );
+	require( 'eventbrite-widgets/eventbrite-widgets.php' );
+	require( 'suggested-pages-setup/suggested-pages-setup.php' );
+	require( 'tlc-transients/tlc-transients.php' );
+	require( 'php-calendar/calendar.php' );
+
+}
+add_action( 'plugins_loaded', 'eventbrite_load_post_keyring' );
